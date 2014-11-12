dc63b8ca87cd8647170de5ea48c7f02f1db3c25d
QuantumGhost <obelisk.reg+bitbucket@gmail.com>
2014-11-12 22:33:32 +0800

    Add ex1.c

---
+++ ex1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	puts("Hello world.");
+	puts("This is a little program.");
+	puts("I'm learning C.");
+	puts("\"This is a mistake.\" She said.");
+	puts("Oh you naughty boy!");
+
+	return 0;
+}
